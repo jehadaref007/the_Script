653589b0269071d2751a46a86767f1fb40a987b1
ALCOPRA007 <49591624+jehadaref007@users.noreply.github.com>
2021-01-08 13:08:57 -0500

    Add files via upload

--- Encrypt.py
+++ Encrypt.py
@@ -7,7 +7,7 @@ md5(), sha1(), sha224(), sha256(), sha384(), sha512(), blake2b(), blake2s()
 import hashlib,termcolor
 # هنا لتلوين
 print(termcolor.colored("1 = md5",'green'      ))
-print(termcolor.colored("2 = sha1",'green'      ))
+print(termcolor.colored("2 = sha1",'green'     ))
 print(termcolor.colored("3 = sha224",'green'   ))
 print(termcolor.colored("4 = sha256",'green'   ))
 print(termcolor.colored("5 = sha384",'green'   ))
@@ -17,10 +17,10 @@ print(termcolor.colored("8 = blake2s",'green'  ))
 
 # طلب من المستخدم نووع التشفير
 
-Encrypt = input('{{!}} enter your type for Encrypt : ' )
+Encrypt = float(input('{^ % ^} enter your type for Encrypt : '))
 # طلب من المستخدم هو النص الذي يريد تشفيره
 
-text = input("{@} enter your text for Encrypt : ")
+text = input("{^ & ^} enter your text for Encrypt : ")
 
 One = hashlib.md5(text.encode()).hexdigest()
 
@@ -40,46 +40,52 @@ eight = hashlib.blake2s(text.encode()).hexdigest()
 
 
 
-if Encrypt == '1':
-    print(termcolor.colored("Succeeded :)",'green'  ))
+if Encrypt == 1:
+    print(termcolor.colored("Succeeded :)", 'green'))
     print(One)
 
-if Encrypt == '2':
-    print(termcolor.colored("Succeeded :)",'green'  ))
+if Encrypt == 2:
+    print(termcolor.colored("Succeeded :)", 'green'))
     print(Two)
 
-if Encrypt =='3':
-    print(termcolor.colored("Succeeded :)",'green'  ))
+if Encrypt ==3:
+    print(termcolor.colored("Succeeded :)", 'green'))
     print(three)
 
-if Encrypt == '4':
-    print(termcolor.colored("Succeeded :)",'green'  ))
+if Encrypt == 4:
+    print(termcolor.colored("Succeeded :)", 'green'))
     print(four)
 
-if Encrypt == '5':
-    print(termcolor.colored("Succeeded :)",'green'  ))
+if Encrypt == 5:
+    print(termcolor.colored("Succeeded :)", 'green'))
     print(five)
 
-if Encrypt =='6':
-    print(termcolor.colored("Succeeded :)",'green'  ))
+if Encrypt == 6:
+    print(termcolor.colored("Succeeded :)", 'green'))
     print(six)
 
-if Encrypt == '7':
-    print(termcolor.colored("Succeeded :)",'green'  ))
+if Encrypt == 7:
+    print(termcolor.colored("Succeeded :)", 'green'))
     print(Seven)
 
-if Encrypt == '8':
-    print(termcolor.colored("Succeeded :)",'green'  ))
+if Encrypt == 8:
+    print(termcolor.colored("Succeeded :)", 'green'))
     print(eight)
-    
+
 
 
 # في حالالمستخدم لم يدخل شي يعطيه هناك خط بلون الاحمر
-if Encrypt =="":
-    print(termcolor.colored(" Not Found :( ", "red"))
+if Encrypt > 8:
+
+    print(termcolor.colored("NOT SUCCEEDED :(", 'red'))
+
+    print(termcolor.colored("Pless Enert NUM (1--8) And Agen", "green"))
 # في حال المستخدم ادخل مساحة يعطيه هناك خطا بلون الاحمر
-if Encrypt ==" ":
-    print(termcolor.colored(" Not Found :( ", "red"))
+if Encrypt < 1:
+
+    print(termcolor.colored("NOT SUCCEEDED :(", 'red'))
+
+    print(termcolor.colored("Pless Enert NUM (1--8) And agen","green"))
 
 
-print(termcolor.colored(" The property rights By {ALCOPRA} ", "red",'on_white'))
+print(termcolor.colored(" The property rights By {Jehad Mosa} ", "red",'on_white'))
